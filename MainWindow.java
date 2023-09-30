cf980c195652f98e186a9d866d5a8322f7582d79
Ryan Reilly <76260911+ReillyLP@users.noreply.github.com>
2023-09-30 18:32:16 -0500

    Update MainWindow.java
    Added the ability to load and save movie entries using a .txt file. 
    Added titleList. 
    Removed text box on bottom of window previously used for troubleshooting.
    Edited text used for tool-tips.
    Fixed if-statement that allowed for movie to be added with blank title.

--- MainWindow.java
+++ MainWindow.java
@@ -12,13 +12,21 @@ import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JLabel;
+import javax.swing.JPopupMenu;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class MainWindow extends JFrame {
 
@@ -40,15 +48,11 @@ public class MainWindow extends JFrame {
 			}
 		});
 	}
-	
-
-	
 
 	/**
 	 * Create the frame.
 	 */
 	public MainWindow() {
-					
 		final int MAX_LIST_SIZE = 50;
 		//HashMap used to store Movie objects using their titles as keys
 		//Initial max. capacity of 50 Movies
@@ -64,6 +68,8 @@ public class MainWindow extends JFrame {
 		ArrayList<String> thrillerList = new ArrayList<String>(MAX_LIST_SIZE);
 		ArrayList<String> dramaList = new ArrayList<String>(MAX_LIST_SIZE);
 		ArrayList<String> otherList = new ArrayList<String>(MAX_LIST_SIZE);
+		
+		ArrayList<String> titleList = new ArrayList<String>(MAX_LIST_SIZE);
 				
 		setResizable(false);
 		setTitle("MainWindow");
@@ -80,13 +86,9 @@ public class MainWindow extends JFrame {
 		textAreaMovieTitle.setFont(new Font("Tahoma", Font.BOLD, 16));
 		textAreaMovieTitle.setWrapStyleWord(true);
 		textAreaMovieTitle.setLineWrap(true);
-		textAreaMovieTitle.setToolTipText("Type title of movie here");
+		textAreaMovieTitle.setToolTipText("Type movie title here");
 		textAreaMovieTitle.setBounds(160, 36, 221, 71);
 		contentPane.add(textAreaMovieTitle);
-		
-		JTextPane textPaneTroubleshootingPrintout = new JTextPane();
-		textPaneTroubleshootingPrintout.setBounds(10, 278, 522, 45);
-		contentPane.add(textPaneTroubleshootingPrintout);
 
 		//Creates a list model of genres to be displayed to user via JList
 		DefaultListModel<String> listModGenre = new DefaultListModel<String>();
@@ -153,34 +155,108 @@ public class MainWindow extends JFrame {
 				listRating.clearSelection();
 			}
 		});
-		btnEntryReset.setToolTipText("Stop an in-progress movie addition and clear fields for next movie");
-		btnEntryReset.setBounds(226, 188, 89, 23);
+		btnEntryReset.setToolTipText("Stop an in-progress movie addition and clear fields");
+		btnEntryReset.setBounds(226, 186, 89, 23);
 		contentPane.add(btnEntryReset);
 
 		JButton btnAddMovie = new JButton("Add Movie");
-		btnAddMovie.setToolTipText("Click after selecting all required movie information to add entry to database");
+		btnAddMovie.setToolTipText("Click after selecting all required movie categoriese to add movie to database");
 		btnAddMovie.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnAddMovie.setFocusPainted(false);
 		btnAddMovie.setHorizontalTextPosition(SwingConstants.CENTER);
-		btnAddMovie.setBounds(211, 111, 120, 34);
+		btnAddMovie.setBounds(211, 118, 120, 33);
 		contentPane.add(btnAddMovie);
 		
-		btnAddMovie.addActionListener(new ActionListener() {
+		//Saves data from each Movie in a .txt file
+		//Iterates through titleList for movieHash keys
+		//tilde(~) used to split values to prevent splitting of titles with commas
+		JButton btnSave = new JButton("Save to File");
+		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//If title, genre, and rating have been selected, construct new Movie and add to HashMap
-				if(textAreaMovieTitle.getText() != null && !listGenre.isSelectionEmpty() 
-				  && !listRating.isSelectionEmpty()) {
-					//Adds new Movie to HashMap, uses index of star rating to determine int value
-					movieHash.put(textAreaMovieTitle.getText(), new Movie(textAreaMovieTitle.getText(), 
-					  listGenre.getSelectedValue(), listRating.getSelectedIndex() + 1));
-					
-					//TODO: add movie to category lists	
-					//FIXME: TEST: used to verify that movie was added to HashMap,
-					//searches Hashmap for title currently displayed in Movie Title box
-					textPaneTroubleshootingPrintout.setText("Movie Added Successfully!\n" 
-					  + movieHash.get(textAreaMovieTitle.getText()).toString());
+				try {
+					PrintWriter saveToTxt = new PrintWriter("SavedMovies.txt");
+					//Print title, genre, and rating of each Movie on a separate line
+					Movie tempMovie;
+					for(String title : titleList) {
+						tempMovie = movieHash.get(title);
+						saveToTxt.println(tempMovie.getTitle() + " ~ " + tempMovie.getGenre() + " ~ " 
+							+ tempMovie.getStarRating());
+					}
+					saveToTxt.close();
+					JOptionPane.showMessageDialog(contentPane, 
+							"Movies Saved to File!");
+				} catch (Exception e1) {
+					JOptionPane.showMessageDialog(contentPane, 
+							"ERROR: Unable to Save Movies to File");
 				}
 			}
 		});
+		
+		btnSave.setToolTipText("Click to save movies to file");
+		btnSave.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnSave.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnSave.setFocusPainted(false);
+		btnSave.setBounds(211, 241, 120, 33);
+		contentPane.add(btnSave);
+		
+		JButton btnLoad = new JButton("Load File");
+		btnLoad.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File movieFile = new File("SavedMovies.txt");
+				Scanner fileReader;
+				try {
+					fileReader = new Scanner(movieFile);
+					
+					while(fileReader.hasNextLine()) {
+						String movieData = fileReader.nextLine();
+						//Splits line into individual movie variables
+						//tilde(~) used to split values to prevent splitting of titles containing commas
+						//Elements are as follows: [0]==title, [1]==genre, [2]==starRating
+						String[] movieVars = movieData.split(" ~ ");
+						//Adds each Movie to the HashMap
+						movieHash.put(movieVars[0], new Movie(movieVars[0], movieVars[1], Integer.parseInt(movieVars[2])));
+						//Adds each Movie to the titleList
+						titleList.add(movieVars[0]);
+					}
+					JOptionPane.showMessageDialog(contentPane, 
+							"Movies Loaded Successfully!");
+				} catch (FileNotFoundException e1) {
+					JOptionPane.showMessageDialog(contentPane, 
+							"ERROR: File Not Found");
+				}
+			}
+		});
+		
+		btnLoad.setToolTipText("Click to load movies from existing file");
+		btnLoad.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnLoad.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnLoad.setFocusPainted(false);
+		btnLoad.setBounds(211, 285, 120, 33);
+		contentPane.add(btnLoad);
+		
+		//TODO: Add movies to their respective category lists
+		btnAddMovie.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//If title, genre, and rating have been selected, construct new Movie and add to HashMap
+				if(!textAreaMovieTitle.getText().isBlank() && !listGenre.isSelectionEmpty() 
+				  && !listRating.isSelectionEmpty()) {
+					String movieTitle = textAreaMovieTitle.getText();
+					//Adds new Movie to HashMap, uses index of star rating to determine int value
+					movieHash.put(movieTitle, new Movie(movieTitle, listGenre.getSelectedValue(), 
+						listRating.getSelectedIndex() + 1));
+					
+					if(titleList.size() < MAX_LIST_SIZE) {
+						titleList.add(movieTitle);
+					}
+					
+					JOptionPane.showMessageDialog(contentPane, textAreaMovieTitle.getText() +
+							" Added Successfully!");
+				}
+				else {
+					JOptionPane.showMessageDialog(contentPane, 
+							"Insufficient Data: Please Provide All Categories");
+				}
+			}
+		});		
 	}
 }
